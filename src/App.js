e519d6fe98657290c4102befe68f3755d6bcdaff
nosemasashi <ma.nose9413@gmail.com>
2020-10-19 22:17:15 +0900

    changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+//JSXでのコーディング
+//直感的にコーディングできる
 function App() {
-  return <div><p>Hello, World!</p></div>;
+  //JSX = JavaScript XML(JSを拡張した言語) 
+  return <h1>Hello, World!</h1>;
 }
+
+//バニラJSでのコーディング↓
+// class App extends Component{
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, World!!!"
+//     )
+//   }
+// }
 
 export default App;
