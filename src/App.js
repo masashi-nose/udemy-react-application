5fa5c9099e0dd93fa79ae358815f8efaa5099084
nosemasashi <ma.nose9413@gmail.com>
2020-10-19 22:56:55 +0900

    learned component

--- src/App.js
+++ src/App.js
@@ -4,29 +4,48 @@ import React from 'react';
 //直感的にコーディングできる
 function App() {
   //JSX = JavaScript XML(JSを拡張した言語) 
-
   // const greeting = "Hi, nice to meet you";
   // const dom = <h1 className="foo">{greeting}</h1>
   //returnで返すJSXは一つのタグで囲む
+  
+  const profiles = [
+    {
+      name: "Taro",
+      age: 20
+    },
+    {
+      name: "Hanako",
+      age: 23
+    },
+    {
+      name: "Noname"
+    }
+  ]
+
   return (
     // <React.Fragment>
     //   <label htmlFor="bar">bar</label>
     //   <input type="text" onClick={() => {console.log("I am clicked");}} />
     // </React.Fragment>
 
+    //props定義
     <div>
-      <Cat/>
-      <Cat/>
-      <Cat/>
-      <Cat/>
-
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
 
   )
 }
 
-const Cat = () => {
-  return <h2>Meow!</h2>
+const User = (props) => {
+  return <h2>I'm {props.name} and {props.age} years old.</h2>
+}
+
+User.defaultProps = {
+  age: 10
 }
 
 //バニラJSでのコーディング↓
